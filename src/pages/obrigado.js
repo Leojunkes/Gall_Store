9d2301f341b65ccd583c80e9a99ed54cf216e67c
Leonardo <leojn8@gmail.com>
2022-02-07 23:57:43 -0300

    atual

---
+++ src/pages/obrigado.js
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/react';
+
+const Thanks = () => {
+  return (
+    <>
+      <Text>Email enviado com sucesso!</Text>
+      <a href="http://localhost:3001/obrigado"></a>
+    </>
+  );
+};
+export default Thanks;
--- src/pages/produtosTESTE.js
+++ src/pages/produtosTESTE.js
@@ -1,78 +1,61 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from '@chakra-ui/react';
+import { Input, Flex, Textarea, Button, FormControl } from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 /* eslint-disable @next/next/link-passhref */
 const TesteFlow = () => {
-  const [cart, setCart] = useState([]);
-  const [dados, setDados] = useState([]);
-  //const cartFormated = cart.map((product) => ({
-  //  ...product,
-  //  pricef: product.valor,
-  //  priceT: product.valor * product.amount,
-  //}));
-
-  const product = [
-    { name: 'almofadas', id: 0, price: 31.99, quantity: 1 },
-    { name: 'almofadas1', id: 1, price: 31.99, quantity: 1 },
-    { name: 'almofadas2', id: 2, price: 31.99, quantity: 1 },
-  ];
-  function add(index) {
-    product[index].quantity += 1;
-  }
-  add(2);
-  add(2);
-  console.log(dados.Produtos);
-
-  const apiTeste = 'http://localhost:3001/api/teste';
-
+  const [cart, setCart] = useState(['']);
   useEffect(() => {
-    axios
-      .get(apiTeste)
-      .then((response) => {
-        setDados(response.data);
-      })
-      .catch((error) => {
-        alert('ocorreu erro durante o get!');
-      });
+    const localStorageProdutos = JSON.parse(localStorage.getItem('products'));
+
+    const cart1 = (localStorageProdutos || []).map((product) => ({
+      ...product,
+      amount: parseInt(product.amount),
+      pricef: parseFloat(product.valor),
+      priceT: parseFloat(product.valor) * parseInt(product.amount),
+    }));
+
+    setCart([...cart1]);
   }, []);
-
-  //useEffect(() => {
-  //  const localStorageProdutos = JSON.parse(localStorage.getItem('products'));
-
-  //  const cart1 =
-  //    localStorage.getItem('products') !== null ? localStorageProdutos : [];
-
-  //  setCart([...cart1]);
-  //}, []);
-
+  console.log(cart);
   return (
-    <Flex>
-      <Box m="25px auto" display="flex">
-        <Button>-</Button>
-        <Text ml="4">1</Text>
-        <Button type="button" onClick={() => add()} ml="4">
-          +
-        </Button>
-      </Box>
-      <Box>
-      <Box>{dados.date}</Box>
-      </Box>
+    <Flex
+      as="form"
+      action="https://formsubmit.co/leojn8@gmail.com"
+      method="POST"
+      flexDirection="column"
+    >
+      <FormControl mt='6'>
+        <Input
+          type="hidden"
+          name="_next"
+          value="http://localhost:3001/obrigado"
+        />
+        <Input type="text" name="name" placeholder="Nome" />
+        <Input type="email" name="email" placeholder="Email Address" />
+        {cart.map((product, key) => (
+          <>
+            <Input
+              key={key}
+              value={product.title}
+              readOnly
+              type="text"
+              name="produto"
+              placeholder="Compras"
+            />
+            <Input
+              value={product.valor}
+              readOnly
+              type="text"
+              name="produto1"
+              placeholder="Compras"
+            />
+          </>
+        ))}
 
-      <Box></Box>
+        <Input h="80px" type="text" placeholder="Mensagem" />
+        <Button type="submit">Enviar</Button>
+      </FormControl>
     </Flex>
   );
 };
